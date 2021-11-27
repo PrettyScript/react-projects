fd2afaf143a311cee6f99ff3f7b94c1021223fd7
PrettyScript <JessicaDelcy@gmail.com>
2021-11-26 18:26:33 -0600

    completed project 12

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
+import { useGlobalContext } from "./context";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
 
 const Sidebar = () => {
+    const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
     return (
-        <aside className={`sidebar show-sidebar`}>
+        <aside
+            className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+        >
             <div className="sidebar-header">
                 <img src={logo} className="logo" alt="coding addict" />
-                <button className="close-btn">
+                <button className="close-btn" onClick={closeSidebar}>
                     <FaTimes />
                 </button>
             </div>
@@ -28,9 +33,11 @@ const Sidebar = () => {
             <ul className="social-icons">
                 {social.map((link) => {
                     const { id, url, icon } = link;
-                    <li key={id}>
-                        <a href={url}>{icon}</a>
-                    </li>;
+                    return (
+                        <li key={id}>
+                            <a href={url}>{icon}</a>
+                        </li>
+                    );
                 })}
             </ul>
         </aside>
